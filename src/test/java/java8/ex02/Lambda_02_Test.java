bdc12aebefb78d79077e4fbffa2b2beca6f50de0
Julien.gazzola <julieng.gazzola@gmail.com>
2018-03-28 15:11:58 +0200

    add ex02

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -5,6 +5,7 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,15 +15,20 @@ import java.util.List;
 public class Lambda_02_Test {
 
     // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
+    interface Mapper<T> {
+        T map(Person p);
     }
     // end::PersonToAccountMapper[]
 
     // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+    private <T> List<T> map(List<Person> personList, Mapper mapper) {
         // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
-        return null;
+    	List<T> resultat = new ArrayList<>();
+    	
+    	for (Person people : personList){
+    		resultat.add((T) mapper.map(people));
+    	}
+        return resultat;
     }
     // end::map[]
 
@@ -35,7 +41,13 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, (Person p) -> {
+				Account account = new Account();
+				
+				account.setBalance(100);
+				account.setOwner(p);
+				return account;
+			});
 
         assert result.size() == personList.size();
         for (Account account : result) {
@@ -50,9 +62,10 @@ public class Lambda_02_Test {
     public void test_map_person_to_firstname() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
+        String name = "";
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> result = map(personList, (Person p) -> p.getFirstname());
 
         assert result.size() == personList.size();
         for (String firstname : result) {
